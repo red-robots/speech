d85820603fbe0daa66edb5089f4a39551536d1bf
Lisa <annalisa.quiamco@gmail.com>
2019-09-10 22:07:03 -0600

    Added function

--- inc/extras.php
+++ inc/extras.php
@@ -377,7 +377,7 @@ function format_phone_number($string) {
     if (strpos($string, '+') !== false) {
         $append = '+';
     }
-    $string = preg_replace("/[^0-9]/", "", "705-666-8888" );
+    $string = preg_replace("/[^0-9]/", "", $string );
     $string = preg_replace('/\s+/', '', $string);
     return $append.$string;
 }
@@ -391,4 +391,29 @@ function get_instagram_setup() {
         $option = '';
     }
     return $option;
+}
+
+function extract_emails_from($string){
+  preg_match_all("/[\._a-zA-Z0-9-]+@[\._a-zA-Z0-9-]+/i", $string, $matches);
+  return $matches[0];
+}
+
+function email_obfuscator($string) {
+    $output = '';
+    if($string) {
+        $emails_matched = ($string) ? extract_emails_from($string) : '';
+        if($emails_matched) {
+            foreach($emails_matched as $em) {
+                $encrypted = antispambot($em,1);
+                $replace = 'mailto:'.$em;
+                $new_mailto = 'mailto:'.$encrypted;
+                $string = str_replace($replace, $new_mailto, $string);
+                $rep2 = $em.'</a>';
+                $new2 = antispambot($em).'</a>';
+                $string = str_replace($rep2, $new2, $string);
+            }
+        }
+        $output = apply_filters('the_content',$string);
+    }
+    return $output;
 }
