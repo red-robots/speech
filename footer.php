efbc9e254b3ef8a7432527a06186ffc8897c21d3
Lisa <annalisa.quiamco@gmail.com>
2019-09-16 23:35:08 -0600

    Styling homepage

--- footer.php
+++ footer.php
@@ -8,6 +8,34 @@
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
+<?php if ( is_front_page() || is_home() ) { ?>
+<script type="text/javascript">
+/* Video Controls */
+jQuery(document).ready(function($){
+	window.onload = function() {
+		// Video
+		if( $("#mp4video").length > 0 ) {
+			var video = document.getElementById("mp4video");
+			document.getElementById('mp4video').play();
+		}
+	}
 
+	if( $("#vimeoVideo").length > 0 ) {
+
+		var video = document.getElementById('vimeoVideo');
+
+	    //Create a new Vimeo.Player object
+	    var player = new Vimeo.Player(video);
+
+	    //When the player is ready, set the volume to 0
+	    player.ready().then(function() {
+	        player.setVolume(0);
+	    });
+	}
+
+});
+
+</script>	
+<?php } ?>
 </body>
 </html>
--- template-parts/content-hero.php
+++ template-parts/content-hero.php
@@ -1,3 +1,50 @@
-<div class="hero">
-	
-</div>
+<?php
+$video = get_field('video_mp4_format');  
+$video_thumb = get_field('video_thumbnail');
+$styles = ($video_thumb ) ? ' style="background-image:url('.$video_thumb['url'].')"':'';
+$video_type = '';
+$query = '';
+$v = '';
+if($video) {
+	$parts = parse_url($video);
+	$base = basename($video);
+	if( isset($parts['query']) && $parts['query'] ) {
+		parse_str($parts['query'], $query);
+		$v = ( isset($query['v']) && $query['v'] ) ? $query['v'] : '';
+	}
+
+	if (strpos($video, 'youtube') > 0) {
+        $video_type = 'youtube';
+    } elseif (strpos($video, 'vimeo') > 0) {
+        $video_type = 'vimeo';
+    } else {
+    	$path = pathinfo($video);
+    	$extension = ( isset($path['extension']) && $path['extension'] ) ? strtolower($path['extension']) : '';
+    	if($extension=='mp4') {
+    		$video_type = 'mp4';
+    	}
+    }
+}
+
+
+?>
+<?php if ( is_front_page() || is_home() ) { ?>
+	<?php if ($video) { ?>
+	<div class="hero">
+		<div class="cover"></div>
+		<div class="videowrap"<?php echo $styles ?>>
+			<?php if ( $video_type=='vimeo' && is_numeric($base) ) { ?>
+				<div style="padding:56.25% 0 0 0;position:relative;"><iframe id="vimeoVideo" src="https://player.vimeo.com/video/<?php echo $base; ?>?autoplay=1&loop=1&title=0&mute=1" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
+			<?php } elseif($video_type=='youtube' && $v) { ?>
+				<div style="padding:56.25% 0 0 0;position:relative;">
+					<iframe type="text/html" src="https://www.youtube.com/embed/<?php echo $v;?>?rel=0&hd=1" frameborder="0" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;"></iframe>
+	  			</div>
+			<?php } elseif($video_type=='mp4') { ?>
+				<video id="mp4video" width="400" height="300" muted playsinline loop>
+					<source src="<?php echo $video; ?>" type="video/mp4">
+				</video>
+			<?php } ?>
+		</div>
+	</div>
+	<?php } ?>
+<?php } ?>
