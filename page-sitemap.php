95750f6495194d47a00703873f47bc7d432e544e
Lisa <annalisa.quiamco@gmail.com>
2019-09-30 22:26:33 -0600

    Added sitemap page

---
+++ page-sitemap.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ * Template Name: Sitemap
+*/
+
+get_header(); ?>
+
+	<div id="primary" class="content-area clear defaultpage">
+		<main id="main" class="site-main wrapper" role="main">
+			
+			<?php while ( have_posts() ) : the_post(); ?> 
+			
+				<header class="page-header text-center">
+					<h1 class="page-title"><?php the_title(); ?></h1>
+				</header><!-- .page-header -->
+				
+				
+				<div class="entry-content text-center">
+					<?php get_template_part('template-parts/content','sitemap'); ?>
+				</div>
+
+			<?php endwhile;  ?>
+
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();
